1fe715a9702633eb79a5dfc2544ddbc185250595
Konstantin <86688686+Lubarskiyk@users.noreply.github.com>
2024-03-12 15:47:49 +0200

    10

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,26 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+  //   const refs = {
+  //     openModalBtn: document.querySelector('[data-modal-open]'),
+  //     closeModalBtn: document.querySelector('[data-modal-close]'),
+  //     modal: document.querySelector('[data-modal]'),
+  //   };
+
+  const burger = {
+    openBurgerBtn: document.querySelector('[data-burger-open]'),
+    closeBurgerBtn: document.querySelector('[data-burger-close]'),
+    burger: document.querySelector('[burger-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  //   refs.openModalBtn.addEventListener('click', toggleModal);
+  //   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-open');
+  burger.openBurgerBtn.addEventListener('click', toggleBurger);
+  burger.closeBurgerBtn.addEventListener('click', toggleBurger);
+
+  //   function toggleModal() {
+  //     refs.modal.classList.toggle('is-open');
+  //   }
+  function toggleBurger() {
+    burger.burger.classList.toggle('is-burger-open');
   }
 })();
