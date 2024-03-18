324805dacd5541699be69f0489f9266933a2720c
Konstantin <86688686+Lubarskiyk@users.noreply.github.com>
2024-03-18 15:09:22 +0200

    99%

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     modal: document.querySelector('#modal'),
     openBtn: document.querySelector('#modal-open'),
     closeBtn: document.querySelector('#modal-close'),
+    body: document.querySelector('body'),
   };
   refs.modal.addEventListener('close', () =>
     refs.modal.classList.remove('modal-open')
@@ -10,17 +11,32 @@
   refs.openBtn.addEventListener('click', () => {
     refs.modal.showModal();
     refs.modal.classList.add('modal-open');
+    burger.body.classList.add('lock');
   });
-  refs.closeBtn.addEventListener('click', () => modal.close());
+  refs.closeBtn.addEventListener('click', () => {
+    refs.modal.close();
+    burger.body.classList.remove('lock');
+  });
 
   const burger = {
     openBurgerBtn: document.querySelector('[data-burger-open]'),
     closeBurgerBtn: document.querySelector('[data-burger-close]'),
-    burger: document.querySelector('[burger-modal]'),
+    burger: document.querySelector('[burger-menu]'),
+    body: document.querySelector('body'),
+    menu: document.querySelectorAll('[header-menu]'),
   };
-  burger.openBurgerBtn.addEventListener('click', toggleBurger);
-  burger.closeBurgerBtn.addEventListener('click', toggleBurger);
-  function toggleBurger() {
-    burger.burger.classList.toggle('is-burger-open');
+
+  burger.openBurgerBtn.addEventListener('click', addBurger);
+  burger.closeBurgerBtn.addEventListener('click', removeBurger);
+  for (i = 0; i < burger.menu.length; i++) {
+    burger.menu[i].addEventListener('click', removeBurger);
+  }
+  function removeBurger() {
+    burger.body.classList.remove('lock');
+    burger.burger.classList.remove('is-burger-open');
+  }
+  function addBurger() {
+    burger.body.classList.add('lock');
+    burger.burger.classList.add('is-burger-open');
   }
 })();
